effe67f7f5fb95a9cedff8028df9ce37f3e0188e
Taiga Doi <s24C1087re@s.chibakoudai.jp>
2025-12-04 23:59:30 +0900

    Fix test.bash source command

--- test/test.bash
+++ test/test.bash
@@ -4,9 +4,13 @@ dir=~
 [ "$1" != "" ] && dir="$1"
 
 cd $dir/ros2_ws
+
+# 1. ビルドする前に、ROS 2の基本環境を読み込む（ここに移動！）
+source /opt/ros/humble/setup.bash
+
 colcon build
 
-source /opt/ros/humble/setup.bash
+# 2. ビルドが終わったら、自分のパッケージの設定を読み込む
 source $dir/ros2_ws/install/setup.bash
 
 timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
