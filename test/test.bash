88f07b1cb645c52e01da92772eb8c3714f7daf37
Taiga Doi <s24C1087re@s.chibakoudai.jp>
2025-12-04 23:28:23 +0900

    Fix test.bash source command

--- test/test.bash
+++ test/test.bash
@@ -5,8 +5,9 @@ dir=~
 
 cd $dir/ros2_ws
 colcon build
-source $dir/ros2_ws/install/setup.bash
-timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
-cat /tmp/mypkg.log |
-grep 'Listen: 10'
+source /opt/ros/humble/setup.bash
+source $dir/ros2_ws/install/setup.bash
+
+timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+grep 'Listen: 10' /tmp/mypkg.log
