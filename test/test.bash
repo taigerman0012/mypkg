ec8f221d75305193e612d1c52db86fc6a25f482a
Taiga Doi <s24C1087re@s.chibakoudai.jp>
2025-12-04 21:54:34 +0900

    Fix test.bash source command

--- test/test.bash
+++ test/test.bash
@@ -5,7 +5,7 @@ dir=~
 
 cd $dir/ros2_ws
 colcon build
-source $dir/.bashrc
+source $dir/ros2_ws/install/setup.bash
 timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
 cat /tmp/mypkg.log |
