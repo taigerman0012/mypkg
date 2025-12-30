eec00039270376f37fda88a31edccbc4643cb8ae
taigerman0012 <s24C1087RE@s.chibakoudai.jp>
2025-12-31 00:40:24 +0900

    fix commnand

--- test/test.bash
+++ test/test.bash
@@ -1,22 +1,15 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2025 Hayato Tsukada
+# SPDX-License-Identifier: BSD-3-Clause
+
 dir=~
 [ "$1" != "" ] && dir="$1"
 
 cd $dir/ros2_ws
-
-# ▼▼▼ 追加する行 ▼▼▼
-# 以前のビルド情報や、rsyncでコピーされた不要なファイルを削除します
-rm -rf build install log
-# ▲▲▲ 追加する行 ▲▲▲
-
-# ROS 2の基本設定
-source /opt/ros/humble/setup.bash
-
-# ビルド実行
 colcon build
+source $dir/.bashrc
+source install/setup.bash  
+timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
-# 自身のパッケージ設定読み込み
-source install/setup.bash
-
-# launch実行
-timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log 
+cat /tmp/mypkg.log |
+grep 'Listen: 10'
